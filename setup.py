f956fa0a1402469271c7e6d11ddcc43601f255ff
Andrea Bergonzo <abergonzo@hubspot.com>
2022-03-05 08:24:14 +0000

    nit setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='ab-raiderio',
       author='amas0',
       description='Python client library for raider.io\'s public API',
       install_requires=[
-            'requests'
+          'requests'
       ])
